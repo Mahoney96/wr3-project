c24ec27c96fb7acde7d6ca5d4e68adb32c16ef75
Mahoney96 <cmahoney966@gmail.com>
2020-08-03 13:42:21 -0700

    Updated images for items

--- server/controller.js
+++ server/controller.js
@@ -3,6 +3,23 @@ const shopitems = require('./shopitems.json')
 
 module.exports = {
     getshopitems: (req, res) => {
-        res.status(200).send(this.getshopitems);
-    }
+        res.status(200).send(getshopitems);
+    },
+    addShopitems: (req, res) => {
+        const { title} = req.body;
+        if (!title) {
+            res.status(405).send('Expect shopitem naming title')
+        } else {
+            const newShopitem = {
+                id,
+                name,
+                inventory,
+                description,
+                //in cart, //
+            };
+            shopitems.push(newShopitem);
+            id++;
+            res.status(200).send(shopitems);
+        }
+    },
 }
